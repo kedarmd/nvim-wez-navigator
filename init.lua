f0771781243e4f41681353c0da6ddcac06f4c974
kedarmd <dindekedar@gmail.com>
2024-12-09 21:50:11 +0530

    fix: fixed the lua errors

--- init.lua
+++ init.lua
@@ -1,12 +1,39 @@
 local wezterm = require("wezterm")
-local move = require("helpers/navigator").move
-
-local M = {}
+local act = wezterm.action
 
 ---@class Keymap
 ---@field key string
 ---@field mods string
 ---@field direction "Left" | "Right" | "Up" | "Down"
+
+---@param window unknown
+---@param command unknown
+---@param key string
+---@param mods string
+local function nvim_wez_nav(window, command, key, mods)
+	local pane = window:active_pane()
+
+	if not pane then
+		wezterm.log_error("No active pane found.")
+		return
+	end
+	local title = pane:get_title()
+	if title == "nvim" then
+		window:perform_action(act.SendKey({ key = key, mods = mods }), pane)
+	else
+		window:perform_action(command, pane)
+	end
+end
+
+--- @param window unknown
+--- @param direction "Left" | "Right" | "Up" | "Down"
+--- @param key string
+--- @param mods string
+local move = function(window, direction, key, mods)
+	nvim_wez_nav(window, act.ActivatePaneDirection(direction), key, mods)
+end
+
+local M = {}
 
 ---@type Keymap[]
 local default_keymaps = {
@@ -24,7 +51,7 @@ function M.keybindings(user_keymaps)
 
 	---@param keymap Keymap
 	local function insert_to_keybindings(keymap)
-		keybindings.insert({
+		table.insert(keybindings, {
 			key = keymap.key,
 			mods = keymap.mods,
 			action = wezterm.action_callback(function(window)
